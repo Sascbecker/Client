353dab172ddaf482602f30b87d026bc849aeeaec
Sascbecker <sascha.becker.mail@icloud.com>
2018-01-15 16:25:34 +0100

    clientgui.1stv

---
+++ ClientGit/src/clientgit/ClientGit.java
@@ -0,0 +1,35 @@
+package clientgit;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author sascha
+ */
+public class ClientGit extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        try{
+        Parent root = FXMLLoader.load(getClass().getResource("/clientgit/Login.fxml"));
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
